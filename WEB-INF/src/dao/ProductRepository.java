f6468f199385ab2a05b1a78e2b38613bc22a1f80
추민영 <20210131chu@sungkyul.ac.kr>
2023-12-02 12:39:32 +0000

    상품목록

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -12,7 +12,7 @@ public class ProductRepository {
     }
 
 	public ProductRepository() {
-		Product Coffee = new Product("a1", "원조커피",2500);
+		Product Coffee = new Product("a1", "원조커피 ",2500);
 		Coffee.setDescription("달콤한 빽다방 대표메뉴, 믹스커피 종결자!");
 		Coffee.setCategory("Coffee");
 		Coffee.setManufacturer("Paiks");
