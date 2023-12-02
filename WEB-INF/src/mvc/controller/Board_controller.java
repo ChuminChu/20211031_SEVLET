db0fa2229c2d4c7c9284076023d4048a5ec22133
추민영 <20210131chu@sungkyul.ac.kr>
2023-12-02 18:13:22 +0000

    controller

--- WEB-INF/src/mvc/controller/Board_controller.java
+++ WEB-INF/src/mvc/controller/Board_controller.java
@@ -3,7 +3,7 @@ package mvc.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
